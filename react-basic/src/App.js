a5dee425488417fd181d2a1e21f9894692483d11
Hiroshi Takeda <takejaponpon+git@gmail.com>
2022-02-23 22:58:08 +0900

    complete react-basic

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -6,17 +6,59 @@ import './App.css';
 // import TimerContainer from './components/TimerContainer';
 import ApiFetch from './components/ApiFetch';
 import AppContext from './contexts/AppContext';
+import B from './components/B'
+import BasicReducer from './components/BasicReducer';
+import CompB from './components/CompB';
+
+import { useReducer, useState, useCallback } from 'react'
+import Memo from './components/Memo';
+import CountDisplay from './components/CountDisplay';
+import CountClick from './components/CountClick';
+const initialState = 0
+const reducer = (currentState, action) => {
+    switch(action) {
+        case 'add_1':
+            return currentState + 1
+        case 'multiple_3':
+            return currentState * 3
+        case 'reset':
+            return initialState
+        default:
+            return currentState
+    }
+}
+
 
 function App() {
+  const [count, dispatch] = useReducer(reducer, initialState)
+  const [count1, setCount1] = useState(0)
+  const [count2, setCount2] = useState(0)
+
+  const AddCount1 = useCallback(() => {
+    setCount1(prevCount => prevCount + 1)
+  }, [count1])
+  const AddCount2 = useCallback(() => {
+    setCount2(prevCount => prevCount + 1)
+  }, [count2])
+
   return (
-    <AppContext.Provider value={'value from App.js'}>
+    <AppContext.Provider value={{countProvided: count, dispatchProvided: dispatch}}>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           {/* <Basic2 /> */}
           {/* <BasicUseEffect /> */}
           {/* <TimerContainer /> */}
-          <ApiFetch />
+          {/* <ApiFetch /> */}
+          {/* <B /> */}
+          {/* <BasicReducer /> */}
+          {/* Count : {count}
+          <CompB /> */}
+          {/* <Memo /> */}
+          <CountDisplay name="count1" count={count1} />
+          <CountClick handleClick={AddCount1}>AddCount1</CountClick>
+          <CountDisplay name="count2" count={count2} />
+          <CountClick handleClick={AddCount2}>AddCount2</CountClick>
         </header>
       </div>
     </AppContext.Provider>
---
+++ react-basic/src/components/B.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import C from './C'
+
+const B = () => {
+    return (
+        <div>
+            <h3>B</h3>
+            <C />
+
+        </div>
+    )
+}
+
+export default B
